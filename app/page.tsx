d3c981024cd899ae58cb7bbd1dc12bb4ab8886cf
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-15 23:50:07 +0200

    tydelig link til oversikt over type bøter

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import {useSpillereOgNavn} from "@/app/hooks/useSpillereOgNavn";
 import SpillerBøter from "@/app/b%C3%B8ter/spiller-bøter";
+import Link from "next/link";
 
 export default function Forside() {
     const {spillere, setSpillere, alleNavn, setAlleNavn, loading, error, setError} = useSpillereOgNavn();
@@ -10,7 +11,9 @@ export default function Forside() {
     if (error) return <p>{error}</p>;
 
     return (
-        <div className="container mx-auto p-4 mt-28 overflow-scroll">
+        <div className="container mx-auto p-4 mt-24 overflow-scroll">
+            <h3 className="text-xl mt-2">Hvilke bøter kan man få?</h3>
+            <Link href={encodeURIComponent("bøter")} className="text-blue-600">Sjekk oversikt her</Link>
             <SpillerBøter setError={setError} spillere={spillere} setSpillere={setSpillere}
                           setAlleNavn={setAlleNavn} alleNavn={alleNavn}/>
         </div>
