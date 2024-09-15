82be38432581a0b53c93adda6e299a4580f3acb2
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-15 13:09:46 +0200

    navbar for mobil

---
+++ app/komponenter/burger-knapp.tsx
@@ -0,0 +1,33 @@
+interface BurgerKnappProps {
+    toggleMenu: () => void;
+}
+
+export default function BurgerKnapp({ toggleMenu }: BurgerKnappProps) {
+    return (
+        <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center sm:hidden p-2 w-10 h-10 justify-center text-gray-800 bg-white
+                rounded px-3 py-2 hover:bg-blue-600 hover:text-white"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+        >
+            <span className="sr-only">Åpne meny</span>
+            <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+            >
+                <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                />
+            </svg>
+        </button>
+    );
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,42 +1,66 @@
+'use client'
+import { useState } from "react";
 import Link from "next/link";
 import Logo from "@/app/ikoner/logo/logo";
+import BurgerKnapp from "@/app/komponenter/burger-knapp";
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className="bg-yellow-300 border-b border-gray-200">
+        <nav className="bg-yellow-300 border-b border-gray-200 relative z-10">
             <div className="max-w-screen-xl flex justify-between items-center mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3">
-                    <Logo/>
+                    <Logo />
                 </Link>
-                <div className="hidden w-full md:flex md:w-auto">
-                    <ul className="flex space-x-8">
-                        <li>
-                            <Link
-                                href="/"
-                                className="text-gray-800 bg-white rounded px-3 py-2 hover:bg-blue-600 hover:text-white"
-                                aria-current="page"
-                            >
-                                Spilleres bøter
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href={'/' + encodeURIComponent('bøter')}
-                                className="text-gray-800 bg-white rounded px-3 py-2 hover:bg-blue-600 hover:text-white"
-                            >
-                                Oversikt typer bøter
-                            </Link>
-                        </li>
-                        {/*<li>*/}
-                        {/*    <Link*/}
-                        {/*        href={'/' + encodeURIComponent('bøter') + '/sjef'}*/}
-                        {/*        className="text-gray-800 bg-white rounded px-3 py-2 hover:bg-blue-600 hover:text-white"*/}
-                        {/*    >*/}
-                        {/*        Botsjef 🔒*/}
-                        {/*    </Link>*/}
-                        {/*</li>*/}
-                    </ul>
+                {/* Burger-knapp for mobil */}
+                <div className="sm:hidden">
+                    <BurgerKnapp toggleMenu={toggleMenu} />
                 </div>
+                {/* Desktop-meny */}
+                <div className="hidden sm:flex space-x-8">
+                    <Link
+                        href="/"
+                        className="text-gray-800 bg-white rounded px-5 py-3 hover:bg-blue-600 hover:text-white"
+                    >
+                        Spilleres bøter
+                    </Link>
+                    <Link
+                        href={'/' + encodeURIComponent('bøter')}
+                        className="text-gray-800 bg-white rounded px-5 py-3 hover:bg-blue-600 hover:text-white"
+                    >
+                        Oversikt typer bøter
+                    </Link>
+                </div>
+            </div>
+
+            {/* Mobil-meny plassert under topplinjen */}
+            <div
+                className={`${isMenuOpen ? 'block' : 'hidden'} sm:hidden bg-yellow-300 w-full absolute p-8 z-20`}
+                id="navbar-mobile"
+            >
+                <ul className="flex flex-col space-y-8 items-center">
+                    <li>
+                        <Link
+                            href="/"
+                            className="text-gray-800 bg-white rounded px-5 py-3 hover:bg-blue-600 hover:text-white"
+                        >
+                            Spilleres bøter
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={'/' + encodeURIComponent('bøter')}
+                            className="text-gray-800 bg-white rounded px-5 py-3 hover:bg-blue-600 hover:text-white"
+                        >
+                            Oversikt typer bøter
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
