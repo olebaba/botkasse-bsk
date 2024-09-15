6828bd9f1694b8fabd22c6a6d4e0900737651544
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-15 11:38:15 +0200

    warning fix

--- app/b%C3%B8ter/sjef/page.tsx
+++ app/b%C3%B8ter/sjef/page.tsx
@@ -3,7 +3,7 @@ import LeggTilBot from "@/app/b%C3%B8ter/sjef/legg-til-bot";
 import {useSpillereOgNavn} from "@/app/hooks/useSpillereOgNavn";
 
 export default function Page() {
-    const {spillere, setSpillere, alleNavn, setAlleNavn, loading, error, setError} = useSpillereOgNavn();
+    const {spillere, setSpillere} = useSpillereOgNavn();
 
     return (
         <LeggTilBot spillere={spillere} setSpillere={setSpillere} />
