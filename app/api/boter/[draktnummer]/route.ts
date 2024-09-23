451ee4db9a6b212e1c5995ea6420b870c4faa882
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-23 20:35:13 +0200

    oppdaterer spiller-tabell

--- app/api/boter/[draktnummer]/route.ts
+++ app/api/boter/[draktnummer]/route.ts
@@ -6,22 +6,28 @@ type Params = {
 }
 
 export async function POST(request: Request, {params}: { params: Params }) {
-    const { beløp, dato, forseelsesId } = await request.json();
+    const {beløp, dato, forseelsesId}: { beløp: number, dato: string, forseelsesId: number } = await request.json();
     const draktnummer = parseInt(params.draktnummer, 10);
 
     if (!draktnummer || !beløp || !dato || !forseelsesId) {
-        return NextResponse.json({ error: 'Alle felter må fylles ut.' }, { status: 400 });
+        return NextResponse.json({error: 'Alle felter må fylles ut.'}, {status: 400});
     }
 
     try {
         await sql`
-      INSERT INTO bøter (draktnummer, beløp, dato, forseelse_id, er_betalt)
-      VALUES (${draktnummer}, ${beløp}, ${dato}, ${forseelsesId}, false)
-    `;
-        return NextResponse.json({ message: 'Bot lagt til' }, { status: 200 });
+            INSERT INTO bøter (draktnummer, beløp, dato, forseelse_id, er_betalt)
+            VALUES (${draktnummer}, ${beløp}, ${dato}, ${forseelsesId}, false)
+        `;
+
+        await sql`
+            UPDATE spillere
+            SET betalt_alle = false, total_sum = total_sum + ${beløp}
+            WHERE draktnummer = ${draktnummer};
+        `
+        return NextResponse.json({message: 'Bot lagt til'}, {status: 200});
     } catch (error) {
         console.error(error);
-        return NextResponse.json({ error: 'Kunne ikke legge til bot.' }, { status: 500 });
+        return NextResponse.json({error: 'Kunne ikke legge til bot.'}, {status: 500});
     }
 }
 
@@ -29,7 +35,7 @@ export async function GET(_request: Request, {params}: { params: Params }) {
     // Hent draktnummer fra params og konverter til number
     const draktnummer = parseInt(params.draktnummer, 10);
     if (isNaN(draktnummer)) {
-        return new Response('Invalid draktnummer', { status: 400 });
+        return new Response('Invalid draktnummer', {status: 400});
     }
 
     // Hent summer
@@ -46,35 +52,35 @@ export async function GET(_request: Request, {params}: { params: Params }) {
 
 // Henter total bøtesum for den siste måneden
 async function hentSumForSisteMaaned(draktnummer: number): Promise<number | null> {
-    const { rows } = await sql`
-    SELECT SUM(beløp) AS total_sum_måned
-    FROM bøter
-    WHERE draktnummer = ${draktnummer}
-    AND dato >= (CURRENT_DATE - INTERVAL '1 month')
-    AND er_betalt = false
-  `;
+    const {rows} = await sql`
+        SELECT SUM(beløp) AS total_sum_måned
+        FROM bøter
+        WHERE draktnummer = ${draktnummer}
+          AND dato >= (CURRENT_DATE - INTERVAL '1 month')
+          AND er_betalt = false
+    `;
     return rows[0]?.total_sum_måned || 0;
 }
 
 // Henter total bøtesum for hele sesongen
 async function hentSumForSesong(draktnummer: number): Promise<number | null> {
-    const { rows } = await sql`
-    SELECT SUM(beløp) AS total_sum_sesong
-    FROM bøter
-    WHERE draktnummer = ${draktnummer}
-    AND dato >= '2024-09-01'  -- Tilpass sesongens startdato
-    AND er_betalt = false
-  `;
+    const {rows} = await sql`
+        SELECT SUM(beløp) AS total_sum_sesong
+        FROM bøter
+        WHERE draktnummer = ${draktnummer}
+          AND dato >= '2024-09-01' -- Tilpass sesongens startdato
+          AND er_betalt = false
+    `;
     return rows[0]?.total_sum_sesong || 0;
 }
 
 // Henter utestående bøter (ikke betalt)
 async function hentUtestaaendeSum(draktnummer: number): Promise<number | null> {
-    const { rows } = await sql`
-    SELECT SUM(beløp) AS utestaaende_sum
-    FROM bøter
-    WHERE draktnummer = ${draktnummer}
-    AND er_betalt = false
-  `;
+    const {rows} = await sql`
+        SELECT SUM(beløp) AS utestaaende_sum
+        FROM bøter
+        WHERE draktnummer = ${draktnummer}
+          AND er_betalt = false
+    `;
     return rows[0]?.utestaaende_sum || 0;
 }
