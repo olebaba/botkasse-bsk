08f04acf0bffe03519102a64c9539b41b14671b6
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-29 16:32:33 +0200

    alert info om vipps

---
+++ app/komponenter/AlertBanner.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface AlertProps{
+    message: string
+    type: 'error' | 'warning' | 'info' | 'success'
+    onClose?: () => void
+}
+
+const AlertBanner = ({ message, type = 'info', onClose }: AlertProps) => {
+    // Define styles based on alert type
+    const typeStyles = {
+        success: 'bg-green-100 text-green-800 border-green-400',
+        error: 'bg-red-100 text-red-800 border-red-400',
+        warning: 'bg-yellow-100 text-yellow-800 border-yellow-400',
+        info: 'bg-blue-100 text-blue-800 border-blue-400',
+    };
+
+    return (
+        <div
+            className={`flex justify-between items-center border-l-4 p-4 ${typeStyles[type]} mb-4`}
+            role="alert"
+        >
+            <span>{message}</span>
+            {onClose && (
+                <button
+                    onClick={onClose}
+                    className="text-lg font-bold ml-4 focus:outline-none"
+                >
+                    &times;
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default AlertBanner;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import SpillerBøter from "@/app/b%C3%B8ter/spiller-bøter";
 import Link from "next/link";
 import {hentSpillere, hentSummerForAlleSpillere} from "@/app/lib/spillereService.ts";
+import AlertBanner from "@/app/komponenter/AlertBanner.tsx";
 
 export const dynamic = 'force-dynamic';
 
@@ -10,6 +11,10 @@ export default async function Forside() {
 
     return (
         <div className="container mx-auto p-4 mt-24">
+            <AlertBanner
+                message="Trykk på raden med ditt draknummer for å betale i Vipps!"
+                type="info"
+            />
             <h3 className="text-xl mt-2">Hvilke bøter kan man få?</h3>
             <Link href={encodeURIComponent("bøter")} className="text-blue-600">Sjekk oversikt her</Link>
             <SpillerBøter spillere={spillereMedSummer}/>
