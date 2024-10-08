f830dd003544efa96b6577179dbd3355f94f3c68
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-08 17:17:41 +0200

    kan overstyre styling på knapp

--- app/komponenter/Knapp.tsx
+++ app/komponenter/Knapp.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 interface KnappProps {
     tekst: string;
     onClick?: () => void;
+    className?: string;
 }
 
-export const Knapp = ({ tekst, onClick }: KnappProps) => {
+export const Knapp = ({ tekst, onClick, className }: KnappProps) => {
+    const defaultClassName = "bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50";
+
     return (
         <button
             type={onClick ? "button" : "submit"}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            className={classNames(className, defaultClassName)}
             onClick={onClick}
         >
             {tekst}
