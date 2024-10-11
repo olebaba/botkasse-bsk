29680fac821562521fa596f9ea02ffec8d13cce6
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-11 18:02:18 +0200

    viser summer i info om spiller

--- app/b%C3%B8ter/spiller-bøter.tsx
+++ app/b%C3%B8ter/spiller-bøter.tsx
@@ -55,14 +55,20 @@ export default function SpillerBøter({spillere, forseelser}: { spillere: Spille
                             {spiller == merInfoSpiller && (
                                 <tr>
                                     <td colSpan={Object.keys(kolonner).length}
-                                        className="p-4 bg-yellow-100 border-b">
-                                        <Header size={"small"} text={`Spiller nummer ${spiller.id}s bøter`}/>
-                                        <Knapp
-                                            tekst={"Betal bøter i Vipps"}
-                                            className="bg-vipps-orange hover:bg-vipps-orange-dark text-white mb-4"
-                                            onClick={() => setSpillerVipps(spiller)}
-                                        />
-                                        <ListBoter forseelser={forseelser} erBotsjef={false} spiller={spiller}/>
+                                        className="p-2 bg-yellow-100">
+                                        <div className="p-2 bg-white rounded">
+                                            <Header size={"small"} text={`Spiller nummer ${spiller.id}s bøter`}/>
+                                            <div className="mb-2 font-semibold">
+                                                <p>Sum alle bøter: {spiller.totalSum + (spiller?.betaltSesong || 0)} kroner.</p>
+                                                <p>Betalt denne sesongen: {spiller.betaltSesong} kroner.</p>
+                                            </div>
+                                            <Knapp
+                                                tekst={"Betal bøter i Vipps"}
+                                                className="bg-vipps-orange hover:bg-vipps-orange-dark text-white mb-4"
+                                                onClick={() => setSpillerVipps(spiller)}
+                                            />
+                                            <ListBoter forseelser={forseelser} erBotsjef={false} spiller={spiller}/>
+                                        </div>
                                     </td>
                                 </tr>
                             )}
