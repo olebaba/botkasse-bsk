d85678ec25c2648bbced238d6bb8840f081567c0
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-30 22:29:36 +0100

    bruker kan logge ut

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -203,3 +203,20 @@ export const validateRequest = cache(
         };
     }
 );
+
+export async function logout(): Promise<void> {
+    "use server";
+    const { session } = await validateRequest();
+    const cookieStore = await cookies();
+    if (!session) {
+        throw {
+            error: "Unauthorized"
+        };
+    }
+
+    await lucia.invalidateSession(session.id);
+
+    const sessionCookie = lucia.createBlankSessionCookie();
+    cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    return redirect("/login");
+}
--- app/minside/page.tsx
+++ app/minside/page.tsx
@@ -1,7 +1,8 @@
-import {validateRequest} from "@/app/lib/auth.ts";
+import {logout, validateRequest} from "@/app/lib/auth.ts";
 import {redirect} from "next/navigation";
 import Header from "@/app/komponenter/Header.tsx";
 import {MinSideInfo} from "@/app/minside/min-side-info.tsx";
+import {Knapp} from "@/app/komponenter/Knapp.tsx";
 
 export default async function MinSide(){
     const {user} = await validateRequest();
@@ -12,6 +13,9 @@ export default async function MinSide(){
         <div className="text-center">
             <Header className="mt-24" size="large" text="Min side" />
             <MinSideInfo brukernavn={user.brukernavn}/>
+            <form action={logout}>
+                <Knapp tekst={"Logg ut"} />
+            </form>
         </div>
     )
 }
