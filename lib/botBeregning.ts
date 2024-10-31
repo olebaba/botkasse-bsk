df57e980dd31916ec6da95e5cb06ffa4a237f6a3
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-11-01 00:59:06 +0100

    retter logikk må betales

--- lib/botBeregning.ts
+++ lib/botBeregning.ts
@@ -1,21 +1,15 @@
 import dayjs from "@/lib/dayjs.ts";
 import type {Bot} from "@/app/api/boter/[spiller_id]/route.ts";
 
-function beregnSumForrigeMaaned(boter: Bot[], betalt: boolean): number {
+export function beregnSumMaaBetales(boter: Bot[]): number {
     const endOfLastMonth = dayjs().subtract(1, "month").endOf("month");
 
     return boter
         .filter(bot =>
-            bot.erBetalt == betalt &&
+            !bot.erBetalt &&
             dayjs(bot.dato).isBefore(endOfLastMonth)
         )
         .reduce((sum, bot) => sum + Number(bot.belop), 0);
-}
-
-export function beregnSumMaaBetales(boter: Bot[]): number {
-    const betaltForrigeMaaned = beregnSumForrigeMaaned(boter, true)
-    const ubetaltForrigeMaaned = beregnSumForrigeMaaned(boter, false)
-    return ubetaltForrigeMaaned - betaltForrigeMaaned
 }
 
 export function beregnSumNyeBoter(boter: Bot[]): number {
