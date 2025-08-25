90b4cbf2e57d8f009f07b72a98d1b09669509cce
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-08-25 23:47:09 +0200

    Oppdaterer bøtesatsvisning for å bruke gjeldende sesong

--- app/boter/page.tsx
+++ app/boter/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { Kontakter } from '@/komponenter/spillere/Kontakter.tsx'
 import type { Forseelse } from '@/app/api/boter/typer/route.ts'
 import { fetchForseelser } from '@/lib/forseelseService.ts'
+import { hentSesongTekst } from '@/lib/botBeregning.ts'
 
 export default function Page() {
     const [forseelser, setForseelser] = useState<Forseelse[]>([])
@@ -16,11 +17,13 @@ export default function Page() {
         hentForseelser().then()
     }, [])
 
+    const gjeldendeSesong = hentSesongTekst()
+
     return (
         <div className="container mx-auto p-4 mt-28">
             <h1 className="text-3xl font-bold mb-6">Oversikt bøter</h1>
             <p>
-                <b>Bøtesatser for sesongen 2024/2025</b>:
+                <b>Bøtesatser for sesongen {gjeldendeSesong}</b>:
             </p>
             <ul className="list-disc ml-4">
                 <li>Gjelder alle spillere med kontrakt ✍️</li>
